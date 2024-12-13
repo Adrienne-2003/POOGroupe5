6af972546e89500ba13bef6f2f690b4d087c692d
Tonymarion6 <demanoumarion@gmail.com>
2024-12-13 22:50:43 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package enrolement;
+
+import java.util.Random;
+
+/**
+ *
+ * @author HP
+ */
+public class Main {
+//    public static void main(String arg[]){
+//        Random rc = new Random();
+//        String a = "ELECT"+rc.nextInt(999999999);
+//        System.out.println(a);
+//    }
+    
+    
+}
